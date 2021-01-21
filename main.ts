4386575625f948c9c798a80e169bb7939d3a760d
Doug Osborn <d00303905@dmail.dixie.edu>
2021-01-21 01:58:16 -0700

    added last player and score as well as top player and score button combos

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString(Player)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString(TopPlayer)
+    basic.showNumber(TopScore)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(highScore)
+})
 radio.onReceivedValue(function (name, value) {
+    highScore = value
+    Player = name
     if (value > TopScore) {
         TopScore = value
         TopPlayer = name
@@ -6,6 +18,8 @@ radio.onReceivedValue(function (name, value) {
     basic.showString(TopPlayer)
     basic.showNumber(TopScore)
 })
+let highScore = 0
+let Player = ""
 let TopPlayer = ""
 let TopScore = 0
 radio.setGroup(12)
